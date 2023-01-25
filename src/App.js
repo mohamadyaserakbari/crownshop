e99794e755819ce9a200f8ebd993900364e50ef9
mohamadyaserakbari <myaser.akbari@gmail.com>
2023-01-25 11:38:55 +0330

    adding navigation bar. adding logo and menu.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
+import Nav from './routes/navigation/nav.component'
 import Home from './routes/home/home.component';
 
+
+
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is Shop</h1>
+    </div>
+  )
+}
+
+
 const App = () => {
-  return(
+  return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Nav />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
@@ -5,32 +6,36 @@ const Home = () => {
     {
       id: 1,
       title: 'Hats',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
+      imageUrl: 'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
     },
     {
       id: 2,
       title: 'Jackets',
-      imageUrl: 'shorturl.at/aDU67'
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
     },
     {
       id: 3,
       title: 'Sneakers',
-      imageUrl: 'shorturl.at/jNUZ1'
+      imageUrl: 'https://images.unsplash.com/photo-1586525198428-225f6f12cff5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
     },
     {
       id: 4,
       title: 'Womans',
-      imageUrl: 'shorturl.at/FJUZ2',
+      imageUrl: 'https://images.unsplash.com/photo-1502163140606-888448ae8cfe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     },
     {
       id: 5,
       title: 'Mens',
-      imageUrl: 'shorturl.at/wGQRY',
+      imageUrl: 'https://images.unsplash.com/photo-1595367555510-0428cca40e16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
     }
   ]
 
   return (
-    <Directory categories={categories} />
+    <div>
+      <Directory categories={categories} />
+      <Outlet />
+    </div>
+    
   );
 }
 
